fd72933621ef78f08967913af4e9d6f079a20af7
Rodrigo <rodrigo.oliveira@inchurch.com.br>
2023-06-02 14:33:21 -0300

    Create notification and reloading list when click.

---
+++ app/src/main/java/com/rodrigo/projeto_imusica_claro/presentation/base/utils/NotificationManager.kt
@@ -0,0 +1,87 @@
+package com.rodrigo.projeto_imusica_claro.presentation.base.utils
+
+import android.Manifest
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.app.ActivityCompat
+
+class NotificationManager(private val context: Context) {
+
+    private fun createNotificationChannel(
+        channelId: String,
+        channelName: String,
+        channelDescription: String
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun sendNotification(
+        channelId: String,
+        title: String,
+        description: String,
+        content: String,
+        smallIconResourceId: Int,
+        largeIcon: Drawable?,
+        colorResourceId: Int,
+        intent: Intent
+    ) {
+        createNotificationChannel(channelId, channelId, description)
+
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setSmallIcon(smallIconResourceId)
+            .setLargeIcon(largeIcon?.toBitmap())
+            .setColor(ContextCompat.getColor(context, colorResourceId))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(context)
+
+        with(notificationManager) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    context as Activity,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    PERMISSION_REQUEST_CODE
+                )
+                return
+            }
+            notify(NOTIFICATION_ID, notification)
+        }
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 1005
+        private const val PERMISSION_REQUEST_CODE = 123
+    }
+}
